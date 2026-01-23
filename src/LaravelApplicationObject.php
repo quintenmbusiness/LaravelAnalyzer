9fe61b2bdd1530cd8ad5802f0580c064db0cd992
quintenmuijser <quintenmbusiness@gmail.com>
2026-01-23 01:20:23 +0100

    Created a method to simply get the laravel application's information that we got, Also massive upgrades on the database structure,  no longer needed to have models present for analysis, automatic relationship analysis works for all types of relationships without needing model class existance
    Created a method to simply get the laravel application's information that we got this currently being all controllers + entire database structure + predictions on how model would be if it did exist if it does exsist it will add path info to it but no data gathering done on it yet,

---
+++ src/LaravelApplicationObject.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace quintenmbusiness\LaravelAnalyzer;
+
+use quintenmbusiness\LaravelAnalyzer\database\objects\DatabaseObject;
+use quintenmbusiness\LaravelAnalyzer\Routes\objects\ControllersObject;
+
+class LaravelApplicationObject
+{
+    /**
+     * @param DatabaseObject    $database
+     * @param ControllersObject $controllers
+     */
+    public function __construct(
+        public DatabaseObject    $database,
+        public ControllersObject $controllers,
+    )
+    {
+    }
+}
---
+++ src/database/objects/DatabaseObject.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace quintenmbusiness\LaravelAnalyzer\database\objects;
+
+use Illuminate\Support\Collection;
+
+class DatabaseObject
+{
+    /**
+     * @var Collection<TableObject>
+     */
+    public Collection $tables;
+
+    public function __construct()
+    {
+        $this->tables = collect();
+    }
+
+    public function resolveModelRelationships(): void
+    {
+        foreach ($this->tables as $table) {
+            foreach($table->model->relations as $modelRelationship) {
+//                $relatedModel = $modelRelationship->relatedTable;
+            }
+        }
+    }
+}
